9a6668fe29f984f41bb8dd83eb7f11440d4df344
wlanboy <me@wlanboy.com>
2019-11-11 14:49:08 +0100

    Added Gorm DB Connection handler

---
+++ model/postgresql.go
@@ -0,0 +1,41 @@
+package model
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
+)
+
+var db *gorm.DB
+
+func init() {
+
+	e := godotenv.Load()
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	username := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	dbName := os.Getenv("db_name")
+	dbHost := os.Getenv("db_host")
+	dbPort := os.Getenv("db_port")
+
+	dbURI := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, username, dbName, password)
+	fmt.Println(dbURI)
+	conn, err := gorm.Open("postgres", dbURI)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	db = conn
+	db.Debug().AutoMigrate(&Event{})
+}
+
+/*GetDB gorm DB connection*/
+func GetDB() *gorm.DB {
+	return db
+}
