a70c387a6ffc6ed0c459a6c498513d82b6bd5599
wlanboy <me@wlanboy.com>
2019-11-12 10:00:12 +0100

    Moved mux handler to own class

---
+++ application/resthandler.go
@@ -0,0 +1,67 @@
+package application
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	model "../model"
+	"github.com/gorilla/mux"
+	uuid "github.com/satori/go.uuid"
+)
+
+/*PostCreate POST method*/
+func (application *GoService) PostCreate(w http.ResponseWriter, r *http.Request) {
+
+	event := model.Event{}
+
+	err := json.NewDecoder(r.Body).Decode(&event)
+	if err != nil {
+		log.Println("Cannot parse JSON")
+		log.Println(err)
+		WriteJSONErrorResponse(w, "Cannot parse JSON", http.StatusBadRequest)
+	} else {
+		errdb, resp := model.SaveEvent(event, application.DB)
+		if errdb != "" {
+			log.Println("Model error")
+			log.Println(errdb)
+			WriteJSONErrorResponse(w, errdb, http.StatusInternalServerError)
+		} else {
+			WriteJSONResponse(w, resp, http.StatusCreated)
+		}
+	}
+}
+
+/*GetByID GET method*/
+func (application *GoService) GetByID(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+	uuid, uuiderr := uuid.FromString(id)
+
+	if uuiderr != nil {
+		WriteJSONErrorResponse(w, "Cannot parse UUID", http.StatusBadRequest)
+	} else {
+		errdb, resp := model.GetEventByID(uuid, application.DB)
+		if errdb != "" {
+			log.Println("Model error")
+			log.Println(errdb)
+			WriteJSONErrorResponse(w, errdb, http.StatusNotFound)
+		} else {
+			WriteJSONResponse(w, resp, http.StatusOK)
+		}
+	}
+}
+
+/*GetAll GET method*/
+func (application *GoService) GetAll(w http.ResponseWriter, r *http.Request) {
+
+	errdb, resp := model.GetAllEvents(application.DB)
+	if errdb != "" {
+		log.Println("Model error")
+		log.Println(errdb)
+		WriteJSONErrorResponse(w, errdb, http.StatusNotFound)
+	} else {
+		WriteJSONArrayResponse(w, resp, http.StatusOK)
+	}
+}
