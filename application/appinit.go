b34c886689a78923ac665245b1647c64fb44cc7d
wlanboy <me@wlanboy.com>
2020-11-14 08:19:45 +0100

    added dotenv support and switch to disable cloud config server

--- application/appinit.go
+++ application/appinit.go
@@ -1,8 +1,12 @@
 package application
 
 import (
+	"log"
+	"os"
+
 	configuration "../configuration"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 /*Initialize app router and configuration*/
@@ -22,16 +26,32 @@ func (goservice *GoService) Initialize() {
 }
 
 func handleConfiguration() ConfigParameters {
-	var cloudconfig configuration.CloudConfig
-	cloudconfig = configuration.LoadCloudConfig()
-
 	var appconfig ConfigParameters
 
-	appconfig.DbUser = cloudconfig.PropertySources[0].Source.DbUser
-	appconfig.DbPass = cloudconfig.PropertySources[0].Source.DbPass
-	appconfig.DbName = cloudconfig.PropertySources[0].Source.DbName
-	appconfig.DbHost = cloudconfig.PropertySources[0].Source.DbHost
-	appconfig.DbPort = cloudconfig.PropertySources[0].Source.DbPort
-	appconfig.DbType = cloudconfig.PropertySources[0].Source.DbType
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(".env file missing")
+	}
+	cloudconfigenabled := os.Getenv("cloudconfigenabled")
+
+	if cloudconfigenabled == "false" {
+		appconfig.DbUser = os.Getenv("db_user")
+		appconfig.DbPass = os.Getenv("db_pass")
+		appconfig.DbName = os.Getenv("db_name")
+		appconfig.DbHost = os.Getenv("db_host")
+		appconfig.DbPort = os.Getenv("db_port")
+		appconfig.DbType = os.Getenv("db_type")
+	} else {
+		var cloudconfig configuration.CloudConfig
+		cloudconfig = configuration.LoadCloudConfig()
+
+		appconfig.DbUser = cloudconfig.PropertySources[0].Source.DbUser
+		appconfig.DbPass = cloudconfig.PropertySources[0].Source.DbPass
+		appconfig.DbName = cloudconfig.PropertySources[0].Source.DbName
+		appconfig.DbHost = cloudconfig.PropertySources[0].Source.DbHost
+		appconfig.DbPort = cloudconfig.PropertySources[0].Source.DbPort
+		appconfig.DbType = cloudconfig.PropertySources[0].Source.DbType
+	}
+
 	return appconfig
 }
