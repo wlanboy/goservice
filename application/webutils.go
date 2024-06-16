f0bf0fb5a606f9e91ae17c243bc1b197688fa16a
wlanboy <wlanboy@github.com>
2024-06-16 11:36:12 +0200

    added json mapper for good responses

--- application/webutils.go
+++ application/webutils.go
@@ -15,6 +15,13 @@ func WriteJSONErrorResponse(w http.ResponseWriter, message string, status int) {
 	fmt.Fprintf(w, `{ "error": "%s" }`, message)
 }
 
+/*WriteJSONOkResponse with content type and status code*/
+func WriteJSONInfoResponse(w http.ResponseWriter, message string, status int) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	fmt.Fprintf(w, `{ "Info": "%s" }`, message)
+}
+
 /*WriteJSONResponse with content type and status code*/
 func WriteJSONResponse(w http.ResponseWriter, event *model.Event, status int) {
 	w.Header().Add("Content-Type", "application/json")
