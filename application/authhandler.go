5c52f9b3cbd77e1efdf191be7acf5d98da656ac0
wlanboy <wlanboy@github.com>
2024-06-16 11:35:40 +0200

    added paths without auth

--- application/authhandler.go
+++ application/authhandler.go
@@ -10,17 +10,28 @@ import (
 /*authMiddleware*/
 func (goservice *GoService) authMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		realm := goservice.Config.Realm
-		userhash := goservice.Config.RealmUser
-		secrethash := goservice.Config.RealmSecret
-		user, pass, ok := r.BasicAuth()
-		if !ok || checkRealmAuthError(user, pass, realm, userhash, secrethash) {
-			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
-			w.WriteHeader(401)
-			w.Write([]byte("You are Unauthorized to access the application.\n"))
-			return
+
+		noAuthPaths := map[string]bool{
+			"/health":  true,
+			"/metrics": true,
+			"/long":    true,
 		}
-		next.ServeHTTP(w, r)
+
+		if noAuthPaths[r.URL.Path] {
+			next.ServeHTTP(w, r)
+		} else {
+			realm := goservice.Config.Realm
+			userhash := goservice.Config.RealmUser
+			secrethash := goservice.Config.RealmSecret
+			user, pass, ok := r.BasicAuth()
+			if !ok || checkRealmAuthError(user, pass, realm, userhash, secrethash) {
+				w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+				w.WriteHeader(401)
+				w.Write([]byte("You are Unauthorized to access the application.\n"))
+				return
+			}
+			next.ServeHTTP(w, r)
+		}
 	})
 }
 
